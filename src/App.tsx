9dd2a0bbd51be19f7eb814dc944e05e97ecf0dd0
Dima12411 <fixer40delta@gmail.com>
2022-02-12 15:31:17 +0300

    Button to switch the clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import DigitalClock from "./components/DigitalClock";
+import styles from './components/Clock.module.css'
+import AnalogClock from "./components/AnalogClock";
 
 function App() {
-  return (
-    <div className="App">
-      <DigitalClock/>
-    </div>
-  );
+    const [change, setChange] = useState<boolean>(true)
+
+    return (
+        <div className={styles.App}>
+            <div>
+                {change ? <AnalogClock/> : <DigitalClock/>}
+            </div>
+            <div className={styles.button}>
+                <button onClick={() => setChange(!change)}>Change to clock</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
